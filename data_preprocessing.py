244dea960738186df3d8cb02f852ab9c6d666cf2
httpsyaseen <yaseenwalker1@gmail.com>
2024-08-15 19:36:43 +0500

    added documentation

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -37,7 +37,7 @@ from sklearn.preprocessing import StandardScaler;
 sc=StandardScaler();
 X_train[:,3:]=sc.fit_transform(X_train[:,3:]);
 X_test[:,3:]=sc.fit_transform(X_test[:,3:]);
-
+  
 print(X_train)
 print(X_test)
 
--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -15,15 +15,16 @@ X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=1)
 
 #Training the model with simple Linear Regression
 from sklearn.linear_model import LinearRegression;
-regressor=LinearRegression();
-regressor.fit(X_train,Y_train);
+model=LinearRegression();
+model.fit(X_train,Y_train);
 
 #Predicting the results by giving the Sample Test Data;
-Y_predict=regressor.predict(X_test)
+Y_predict=model.predict(X_test)
+print(Y_predict)
 
 #Visualizing the Train Data
 plt.scatter(X_train,Y_train,color='red');
-plt.plot(X_train,regressor.predict(X_train),color='blue');
+plt.plot(X_train,model.predict(X_train),color='blue');
 plt.title('Salary vs Experience');
 plt.xlabel('Years of Experience');
 plt.ylabel('Salary');
@@ -31,13 +32,23 @@ plt.show();
 
 #Visualizing the Test Data
 plt.scatter(X_test,Y_test,color='red');
-plt.plot(X_train,regressor.predict(X_train),color='blue');
+plt.plot(X_train,model.predict(X_train),color='blue');
 plt.title('Salary vs Experience');
 plt.xlabel('Years of Experience');
 plt.ylabel('Salary');
 plt.show();
 
 
+#(Q1)For Single Prediction for a Employee with 12Years of Experience
+#Here this function Expect a 2D array
+# 12→scalar 
+# [12]→1D array 
+# [[12]]→2D array
+# Y_predict=model.predict([[12]])
+# print(Y_predict)
 
-
+#(Q2)How do I get the final regression equation y = b0 + b1 x with the final values of the coefficients b0 and b1?
+# print(model.coef_)(b1)
+# print(model.intercept_)(b0)
+# Salary=26816.19+9345.94×YearsExperience
 
